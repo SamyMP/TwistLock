4d29966104609002395ac7e83deca5fd634fd78c
jonatjano <jonathan.selle@laposte.net>
2018-03-25 21:07:01 +0200

    changed value to prevent crash with random sized game

--- src/twistlock/Controller.java
+++ src/twistlock/Controller.java
@@ -67,12 +67,12 @@ public class Controller
 			}
 		}
 
-		containers[1][8].capture(1, this.players[0]);
-		containers[1][7].capture(3, this.players[1]);
-		containers[1][7].capture(1, this.players[0]);
-		containers[3][6].capture(1, this.players[1]);
-		containers[1][7].capture(2, this.players[0]);
-		containers[3][6].capture(4, this.players[1]);
+		containers[1][4].capture(1, this.players[0]);
+		containers[1][3].capture(3, this.players[1]);
+		containers[1][3].capture(1, this.players[0]);
+		containers[3][2].capture(1, this.players[1]);
+		containers[1][3].capture(2, this.players[0]);
+		containers[3][2].capture(4, this.players[1]);
 		window.repaint();
 		System.out.println(containers[0][7].getPlayer());
 	}
