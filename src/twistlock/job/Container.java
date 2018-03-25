a38bef9126ece7f335298d8069011610ffbb5fa0
jonatjano <jonathan.selle@laposte.net>
2018-03-25 03:23:12 +0200

    container should work correctly without further modification

--- src/twistlock/job/Container.java
+++ src/twistlock/job/Container.java
@@ -6,21 +6,56 @@ package twistlock.job;
 public class Container
 {
 	/**
+	* the minimal value a container can hold
+	*/
+	private final int MIN_VALUE = 5;
+
+	/**
+	* the maximum value a container can hold
+	*/
+	private final int MAX_VALUE = 54;
+
+	private int player;
+
+	/**
 	 * array containing the {@link Twistlock} of each corner of this container
 	 */
 	private Twistlock[] corners;
 
+	/**
+	 * the row coordinate of the container
+	 */
 	private int row;
 
+	/**
+	 * the column coordinate of the container
+	 */
 	private int column;
 
 	/**
-	 * random beetwen 5 and 54
+	 * the score gained when a player control this container
+	 * is a random beetwen 5 and 54
 	 */
 	private int value;
 
-	public Container()
+	/**
+	 * create a container
+	 */
+	public Container(int row, int column, Twistlock nw, Twistlock ne, Twistlock sw, Twistlock se)
 	{
+		player = -1;
+		this.row = row;
+		this.column = column;
+		corners = new Twistlock[] { nw, ne, sw, se };
+		value = (int)(Math.random() * (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
+	}
 
+	/**
+	 * get the container value
+	 * @return the container value
+	 */
+	public int getValue()
+	{
+		return value;
 	}
 }
